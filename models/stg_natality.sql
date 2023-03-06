dccdd4eb974ed3cd3fdf87b39ae2b2b593114eca
bram.vandenberg@brightcubes.nl <bram.vandenberg@brightcubes.nl>
2023-03-06 10:11:37 +0000

    added natality data

---
+++ models/stg_natality.sql
@@ -0,0 +1,7 @@
+select  Year,
+        County_of_Residence as County,
+        Births,
+        Ave_Age_of_Mother as Age_Mother
+
+from `bigquery-public-data.sdoh_cdc_wonder_natality.county_natality`
+
